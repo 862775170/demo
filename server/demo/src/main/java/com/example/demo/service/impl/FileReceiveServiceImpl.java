818f7198aa71608eba418c0c4bc5b3b06c2993ee
hh <862775170@qq.com>
2019-05-16 09:13:10 +0800

    代码提交

--- server/demo/src/main/java/com/example/demo/service/impl/FileReceiveServiceImpl.java
+++ server/demo/src/main/java/com/example/demo/service/impl/FileReceiveServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.demo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.FileReceiveRepository;
 import com.example.demo.service.FileReceiveService;
 
 @Service
 public class FileReceiveServiceImpl implements FileReceiveService {
 
+	@Autowired
+	private FileReceiveRepository fileReceiveRepository;
 }
--- server/demo/src/main/java/com/example/demo/service/impl/FileSendServiceImpl.java
+++ server/demo/src/main/java/com/example/demo/service/impl/FileSendServiceImpl.java
@@ -1,7 +1,14 @@
 package com.example.demo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.FileSendRepository;
 import com.example.demo.service.FileSendService;
 
+@Service
 public class FileSendServiceImpl implements FileSendService {
 
+	@Autowired
+	private FileSendRepository fileSendRepository;
 }
