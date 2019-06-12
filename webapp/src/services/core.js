a595397c23d254b0ddad300498aff0eddaf6c057
lw <1977044992@qq.com>
2019-06-12 13:49:05 +0800

    lw

--- webapp/src/services/core.js
+++ webapp/src/services/core.js
@@ -22,6 +22,7 @@ export async function fileList(params){
   return request('/api/sw/file/list', {
     method: 'POST',
     body: {
+      pageSize:10000000,
       ...params,
       method: 'post',
     },
